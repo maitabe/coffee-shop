d8d81dde9f7aa31fb9db470c58dc98f5cab6b44f
maitabe <martushb@gmail.com>
2016-11-06 01:06:20 +0200

    coffee shop program adding methods and properties to the object

--- main.js
+++ main.js
@@ -1,20 +1,86 @@
 var coffeeShop = {
   beans: 40,
+  money: 100,
 
   drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12
+    latte: {
+        beanRequirement:10,
+        price: 8
+    },
+    americano: {
+        beanRequirement:5,
+        price: 4
+    },
+    doubleShot: {
+        beanRequirement:15,
+        price: 6
+    },
+    frenchPress: {
+        beanRequirement:12,
+        price: 11
+    }
   },
 
   makeDrink: function (drinkType) {
     // TODO: Finish this method
+    var isADrink = false;
+    //loop through the drink requirements
+
+    var req = coffeeShop.drinkRequirements;
+
+
+    for(var coffee in req) {
+      var numBeans = req[coffee].beanRequirement;
+      //check if coffee exist
+      // if(drinkType === coffee && (this.beans > numBeans)){
+      if(drinkType === coffee){
+         isADrink = true;
+         console.log(drinkType + ' on the making' );
+
+        // break;
+        coffeeShop.reduceBeans(numBeans);
+      }
+    }
+
+    //loop is over,  check if coffee doesnt exist
+    if(isADrink === false) {
+      // alert('Sorry, we don\'t make ' + drinkType + ' coffee');
+      console.log('Sorry, we don\'t make ' + drinkType + ' coffee');
+    }
+
+  },
+  //reduce amount of beans per drink
+  reduceBeans: function (numBeans) {
+    this.beans -= numBeans;
+
+    console.log('coffee beans left ' + this.beans);
+    if(this.beans < 5) {
+      // alert('Sorry, we\'re all out of beans!');
+      console.log('Sorry, we\'re all out of beans!');
+    }
+  },
+
+  buySupplies: function (coins) {
+    var beansPrice = 3;
+    var totalCost = coins * beansPrice;
+    //deduct the money with total cost
+    this.money -= totalCost;
+  },
+
+  buyDrink: function (price) {
+    this.money += price;
+
+    this.makeDrink();
   }
-}
+};
 
 coffeeShop.makeDrink("latte");
 coffeeShop.makeDrink("americano");
 coffeeShop.makeDrink("filtered");
 coffeeShop.makeDrink("doubleShot");
 coffeeShop.makeDrink("frenchPress");
+
+
+//fix bug when coffee beans runs out
+//check INDIVIDUAL EXERCISE (EXTENSION 2)
+//solve INDIVIDUAL EXERCISE (EXTENSION 3)
